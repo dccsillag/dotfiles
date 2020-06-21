afaf466c01103bbf2aec969eaabbc06d29690499
dccsillag <dccsillag@gmail.com>
2020-06-21 01:01:58 -0300

    Add .csillag installation scripts

---
+++ .csillag/install-packages.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Stop on errors
+set -e
+
+PACKAGEFILE=$HOME/.csillag/packages
+
+if [ -z "$DISTRONAME" ]
+then
+    DISTRONAME=$(lsb_release -is)
+fi
+
+STARTLINENUM=$(grep -m 1 -n '^-' "$PACKAGEFILE" | sed 's/^\([0-9]\+\):.\+$/\1/')
+DISTROS=$(sed "$((STARTLINENUM-1))q;d" "$PACKAGEFILE")
+DISTRONUM=$(echo "$DISTROS" | sed 's/ \+/\n/g' | grep -n "$DISTRONAME" | sed 's/^\([0-9]\+\):.\+$/\1/')
+
+if [ -z "$DISTRONUM" ]
+then
+    echo "Your distro ($DISTRONAME) does not appear in '$PACKAGEFILE'."
+    echo "If you want to force it to some particular value, export"
+    echo "  \`DISTRONAME=YourDistroName\` on your shell."
+    exit 2
+fi
+
+PACKAGES=$(tail -n +$((STARTLINENUM+1)) "$PACKAGEFILE" | grep -v '^#' | sed 's/ \+/\t/g' | cut -f "$DISTRONUM" | grep -v _)
+
+echo "Your distro is: $DISTRONAME"
+
+set -x
+
+case $DISTRONAME in
+    Ubuntu)
+        # shellcheck disable=SC2086 # these word splits are intentional
+        apt install $PACKAGES
+        ;;
+    ManjaroLinux)
+        # shellcheck disable=SC2086,SC2046 # these word splits are intentional
+        yay -Syu $(echo $PACKAGES | tr '\n' ' ')
+        ;;
+    *)
+        echo "Unregistered distro: $DISTRONAME"
+        exit 2
+esac
--- .github/README.md
+++ .github/README.md
@@ -1,4 +1,26 @@
 dotfiles
 ========
 
-This a Git Bare repository with my dotfiles!
+This a Bare repository with my dotfiles.
+
+The following Linux distributions are supported:
+
+- Ubuntu
+- Manjaro (this likely implies Arch as well)
+
+Installation
+------------
+
+First, clone this repository (keep in mind this is a Git Bare repository):
+
+```sh
+cd
+git clone --bare git@github.com:dccsillag/dotfiles.git
+```
+
+Next, run the install script (this will take a while):
+
+```sh
+cd ~/.csillag
+./install-packages.sh
+```
