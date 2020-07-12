66158c24fbcc6cdd4372d81c9b0f63c305629c45
dccsillag <dccsillag@gmail.com>
2020-07-11 22:21:14 -0300

    Add qutebrowser config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,25 @@
+from qutebrowser.api import interceptor
+import sys, os
+
+c.tabs.position = 'top'
+
+# c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.enabled = False
+
+c.tabs.last_close = 'close'
+# c.tabs.wrap = False
+c.tabs.background = True
+
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+    'aw': 'https://wiki.archlinux.org/index.php?search={}',
+    'go': 'https://www.google.com/search?q={}',
+    'lib': 'http://gen.lib.rus.ec/search.php?req={}',
+}
+
+# Adblocker
+# sys.path.append(os.path.join(sys.path[0], 'jblock'))
+
+config.source(os.path.expanduser('~/.config/qutebrowser/nord-qutebrowser.py'))
+
+config.load_autoconfig()
