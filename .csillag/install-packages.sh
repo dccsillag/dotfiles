b4d8741778735a9a9db641ed635e4ac1bdf8d593
dccsillag <dccsillag@gmail.com>
2020-07-12 21:57:07 -0300

    Fix problem with missing distro check in .csillag/install.sh

--- .csillag/install-packages.sh
+++ .csillag/install-packages.sh
@@ -39,7 +39,7 @@ fi
 
 STARTLINENUM=$(grep -m 1 -n '^-' "$PACKAGEFILE" | sed 's/^\([0-9]\+\):.\+$/\1/')
 DISTROS=$(sed "$((STARTLINENUM-1))q;d" "$PACKAGEFILE")
-DISTRONUM=$(echo "$DISTROS" | sed 's/ \+/\n/g' | grep -n "$DISTRONAME" | sed 's/^\([0-9]\+\):.\+$/\1/')
+DISTRONUM=$(echo "$DISTROS" | sed 's/ \+/\n/g' | grep -n "^$DISTRONAME$" | sed 's/^\([0-9]\+\):.\+$/\1/')
 
 if [ -z "$DISTRONUM" ]
 then
@@ -52,8 +52,6 @@ fi
 PACKAGES=$(tail -n +$((STARTLINENUM+1)) "$PACKAGEFILE" | grep -v '^#' | sed 's/ \+/\t/g' | cut -f "$DISTRONUM" | grep -v _)
 
 echo "Your distro is: $DISTRONAME"
-
-set -x
 
 case $DISTRONAME in
     Ubuntu)
@@ -77,8 +75,6 @@ case $DISTRONAME in
         echo "Unregistered distro: $DISTRONAME"
         exit 2
 esac
-
-set +x
 
 echo "Running post-install scripts..."
 find "$HOME/.csillag/post-package-install/" -exec /bin/sh '{}' \; -name "$DISTRONAME.sh" -or -name all.sh
