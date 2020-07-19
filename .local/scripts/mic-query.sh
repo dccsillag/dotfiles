c46adf4d733d67521ac7e7654b46de8e0939ad5f
dccsillag <dccsillag@gmail.com>
2020-07-19 12:56:38 -0300

    Change MIC symbol in xmobar

--- .local/scripts/mic-query.sh
+++ .local/scripts/mic-query.sh
@@ -2,7 +2,9 @@
 
 if { pacmd list-sources | grep -q "muted: no"; }
 then
-    echo "<fc=#ff3333></fc>"
+    # echo "<fc=#ff3333></fc>"
+    echo "<fc=#ff4444>MIC</fc>"
 else
-    echo ""
+    # echo ""
+    echo
 fi
