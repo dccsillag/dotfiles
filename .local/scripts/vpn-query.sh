b1a5286a164f10a8161454d4908388314450d642
dccsillag <dccsillag@gmail.com>
2020-09-26 21:38:50 -0300

    Fix vpn query script to avoid false positives

--- .local/scripts/vpn-query.sh
+++ .local/scripts/vpn-query.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ $(ps aux | grep [s]sh | wc -l) -gt 1 ]
+if [ $(ps aux | grep ^ssh | wc -l) -gt 1 ]
 then
     printf " <fc=#ED8A00>"
 else
     printf " <fc=#00AAEE>"
 fi
 
-if ps aux | grep -v "$(basename $0)" | grep [v]pn > /dev/null;
+if pgrep -l vpn | grep -v "$(basename $0)" | cut -d' ' -f 2 | grep [v]pn > /dev/null;
 then
     printf "VPN"
 fi
