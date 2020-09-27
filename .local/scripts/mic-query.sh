f9226d13b528255b2e5da7f306e39abebcabd99b
dccsillag <dccsillag@gmail.com>
2020-09-26 21:39:14 -0300

    Change mic query script and xbindkeys config to ignore pulse monitors

--- .local/scripts/mic-query.sh
+++ .local/scripts/mic-query.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if { pacmd list-sources | grep -q "muted: no"; }
+if { pacmd list-sources | grep 'name:\|muted:' | sed 'N;s/\n/ /' | grep -v monitor | grep -q "muted: no"; }
 then
     # echo "<fc=#ff3333></fc>"
     echo " <fc=#ff4444>MIC</fc>"
