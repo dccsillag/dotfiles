39f64eaf069a021ea10c605d21ba259d338eee7e
Daniel Csillag <dccsillag@gmail.com>
2020-11-18 19:39:38 -0300

    Change default layout in Xmonad (Dwindle -> MosaicAlt)

--- .xmonad/lib/XMonad/Csillag/Layouts.hs
+++ .xmonad/lib/XMonad/Csillag/Layouts.hs
@@ -34,7 +34,7 @@ instance Transformer MAGNIFIER Window where
     transform MAGNIFIER x k = k (magnifier x) $ \(ModifiedLayout _ x') -> x'
 
 myLayouts = draggingVisualizer $ mkToggle (single MAGNIFIER) $
-  renamed [Replace "Dwindle"] (winSpaces $ Dwindle R CW 1 1.1)
+  renamed [Replace "Mosaic"] (winSpaces $ MosaicAlt M.empty)
     ||| renamed [Replace "Mirror Dwindle"]
                 (Mirror $ winSpaces $ Dwindle R CW 1 1.1)
     ||| renamed [Replace "Grid"]
@@ -43,8 +43,7 @@ myLayouts = draggingVisualizer $ mkToggle (single MAGNIFIER) $
     ||| renamed [Replace "OneBig"] (winSpaces $ OneBig (3 / 4) (3 / 4))
     ||| renamed [Replace "ThreeColMid"]
                 (winSpaces $ ThreeColMid 1 (3 / 100) (1 / 2))
-    ||| renamed [Replace "Mosaic"]
-                (winSpaces $ MosaicAlt M.empty)
+    ||| renamed [Replace "Dwindle"] (winSpaces $ Dwindle R CW 1 1.1)
     -- ||| renamed [Replace "Circle"] Circle
  where
   winSpaces = spacingRaw True
