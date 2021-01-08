1a03e4fa7590e2028ad989ddd991c1050c32fc46
Daniel Csillag <dccsillag@gmail.com>
2021-01-08 10:13:15 -0300

    Fix vpn-query script

--- .local/scripts/vpn-query.sh
+++ .local/scripts/vpn-query.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ $(ps aux | grep ^ssh | wc -l) -gt 1 ]
+if pgrep -x ssh > /dev/null
 then
     printf " <fc=#ED8A00>"
 else
     printf " <fc=#00AAEE>"
 fi
 
-if pgrep -l vpn | grep -v "$(basename $0)" | cut -d' ' -f 2 | grep [v]pn > /dev/null;
+if pgrep -l vpn | grep -v "$(basename "$0")" | cut -d' ' -f 2 | grep '[v]pn' > /dev/null;
 then
     printf "  嬨  "
 fi
