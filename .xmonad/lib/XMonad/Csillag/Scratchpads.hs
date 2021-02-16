c93901751e8d2dab9a7faa9e1753d07283c07d6b
Daniel Csillag <dccsillag@gmail.com>
2021-02-16 08:55:36 -0300

    Remove old unused scratchpads in xmonad

--- .xmonad/lib/XMonad/Csillag/Scratchpads.hs
+++ .xmonad/lib/XMonad/Csillag/Scratchpads.hs
@@ -37,22 +37,11 @@ myScratchpads =
        , query = className =? "audiomanage"
        , hook  = floatingScratchpad
        }
-  , NS { name  = "advancedaudio"
-       , cmd   = term_run' "advancedaudio" "alsamixer"
-       , query = className =? "advancedaudio"
-       , hook  = floatingScratchpad
-       }
   , NS { name  = "deezer"
        , cmd   = browser_open "http://deezer.com"
        , query = isSuffixOf "Deezer - qutebrowser" <$> title
        , hook  = floatingScratchpad
        }
-  , NS
-    { name  = "todo"
-    , cmd   = "st -c todo -e sh -c \"nvim $(ls ~/todo/*.toq | tr '\\n' ' ')\""
-    , query = className =? "todo"
-    , hook  = floatingScratchpad
-    }
   , NS
     { name  = "slack"
     , cmd   = "slack"
