ddff322e5330b13ff44d4d5ed6eaacd8ad632c6b
Daniel Csillag <daniel.csillag@impa.br>
2021-03-02 07:32:20 -0300

    Fix python startup.py so that ~/.local/misc/python/ is created if nonexistent

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -3,6 +3,8 @@ import os
 import readline
 
 histfile = os.path.expanduser('~/.local/misc/python/history')
+if not os.path.exists(os.path.dirname(histfile)):
+    os.makedirs(os.path.dirname(histfile))
 try:
     readline.read_history_file(histfile)
     readline.set_history_length(1000)
