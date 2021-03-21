ac1be0b73afd9df83ad97bb8418df95e8faed183
Daniel Csillag <dccsillag@gmail.com>
2021-03-21 08:37:00 -0300

    Change bgset so that it doesn't require the # before a hex color

--- .local/scripts/bgset.sh
+++ .local/scripts/bgset.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-printf '\e]11;%s\a' "$1"
+is_number() {
+    echo "$1" | head -1 | grep '^[0-9]\+$' > /dev/null
+    return $?
+}
+
+if is_number "$1"; then
+    printf '\e]11;#%s\a' "$1"
+else
+    printf '\e]11;%s\a' "$1"
+fi
