8fe7c829b8ff9aeda2dd88480500768cd071002d
Daniel Csillag <dccsillag@gmail.com>
2021-04-10 16:48:09 -0300

    Change new statusbar colors

--- .local/scripts/statusbar/main.sh
+++ .local/scripts/statusbar/main.sh
@@ -20,7 +20,7 @@ bar_left() {
     # CPU
     cpu_usage="$(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print 100 - $1}')"
     append "﬙ "
-    test "$(printf "%.0f" "$cpu_usage")" -gt 90 && append "^fg(red)"
+    test "$(printf "%.0f" "$cpu_usage")" -gt 90 && append "^fg(tomato)"
     printf "%3.0f%%" "$cpu_usage"
 
     block_sep
@@ -33,7 +33,7 @@ bar_left() {
     mem_usage=$((100*used_mem / total_mem))
     mem_cache=$((100*cache_mem / total_mem))
     append " "
-    test "$mem_usage" -gt 90 && append "^fg(red)"
+    test "$mem_usage" -gt 90 && append "^fg(tomato)"
     printf "%3d%%" "$mem_usage"
     append "^fg()"
     printf " +%3d%%" "$mem_cache"
@@ -46,7 +46,7 @@ bar_left() {
     used_swap=$(echo "$swaps" | awk '{print $3}' )
     swap_usage=$((100*used_swap / total_swap))
     append "易 "
-    test "$swap_usage" -gt 80 && append "^fg(red)"
+    test "$swap_usage" -gt 80 && append "^fg(tomato)"
     printf "%3d%%" "$swap_usage"
 
     block_sep
@@ -54,7 +54,7 @@ bar_left() {
     # Temperature
     max_temp="$(sensors | grep '^.\+:.\+°C' | sed 's/^.\+: *+\([0-9.]\+\)°C.\+$/\1/' | sort -g | tail -1)"
     append "﨎 "
-    test "$(printf "%.0f" "$max_temp")" -gt 85 && append "^fg(red)"
+    test "$(printf "%.0f" "$max_temp")" -gt 85 && append "^fg(tomato)"
     printf "%3.0f°C" "$max_temp"
 }
 
@@ -67,7 +67,6 @@ bar_right() {
     # Do not disturb
     if [ -f /home/daniel/.dunst_paused ]
     then
-        append "^fg(#00aa88)"
         append ""
         block_sep
     fi
@@ -77,20 +76,15 @@ bar_right() {
         SCREEN_SHARING=$(xwininfo -root -children | grep "[a-z.]\+ is sharing your screen" | awk '{print $1}')
         WINDOW_SHARING=$(xwininfo -root -children | grep "[a-z.]\+ is sharing a window" | awk '{print $1}')
 
-        test -n "$SCREEN_SHARING" && test -z "$WINDOW_SHARING" && append "^fg(#f29407)  "      && block_sep
-        test -z "$SCREEN_SHARING" && test -n "$WINDOW_SHARING" && append "^fg(#f29407) 缾"      && block_sep
-        test -n "$SCREEN_SHARING" && test -n "$WINDOW_SHARING" && append "^fg(#f29407) 缾   " && block_sep
+        test -n "$SCREEN_SHARING" && test -z "$WINDOW_SHARING" && append "^fg(darkorange)  "      && block_sep
+        test -z "$SCREEN_SHARING" && test -n "$WINDOW_SHARING" && append "^fg(darkorange) 缾"      && block_sep
+        test -n "$SCREEN_SHARING" && test -n "$WINDOW_SHARING" && append "^fg(darkorange) 缾   " && block_sep
     )
 
     # VPN
     if pgrep -l vpn | grep -v "$(basename "$0")" | cut -d' ' -f 2 | grep '[v]pn' > /dev/null;
     then
-        if pgrep -x ssh > /dev/null
-        then
-            append "^fg(#ED8A00)"
-        else
-            append "^fg(#00AAEE)"
-        fi
+        pgrep -x ssh > /dev/null && append "^fg(yellow)"
         append "嬨"
         block_sep
     fi
@@ -98,10 +92,9 @@ bar_right() {
     # Mic
     if { pacmd list-sources | grep 'name:\|muted:' | sed 'N;s/\n/ /' | grep -v monitor | grep -q "muted: no"; }
     then
-        append "^fg(red)"
+        append "^fg(tomato)"
         append ""
     else
-        append "^fg(gray)"
         append ""
     fi
 
@@ -135,7 +128,7 @@ bar_right() {
         icon="?"
     fi
     append "$icon "
-    test "$battpercent" -lt 10 && append "^fg(red)"
+    test "$battpercent" -lt 10 && append "^fg(tomato)"
     printf "%2d%%" "$battpercent"
 }
 
