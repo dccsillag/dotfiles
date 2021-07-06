529eba86426bd05099cf6902a286bd5a1b102917
Daniel Csillag <dccsillag@gmail.com>
2021-07-06 13:53:04 -0300

    Add window spacing in XMonad

--- .xmonad/lib/XMonad/Csillag/Layouts.hs
+++ .xmonad/lib/XMonad/Csillag/Layouts.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances #-}
-
 module XMonad.Csillag.Layouts
     ( myLayouts
     )
@@ -8,7 +6,6 @@ where
 import XMonad hiding ((|||))
 
 import XMonad.Layout.LayoutCombinators ((|||))
-import qualified Data.Map as M
 
 import XMonad.Layout.Renamed
 import XMonad.Layout.IfMax
@@ -19,11 +16,12 @@ import XMonad.Layout.OneBig
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.StackTile
 
+import XMonad.Layout.Spacing (spacingRaw, Border(..))
 import XMonad.Layout.DraggingVisualizer
 import XMonad.Layout.Maximize
 
 
-myLayouts = draggingVisualizer $ maximize $
+myLayouts = draggingVisualizer $ maximize $ spacing $
     renamed [Replace "Grid"]           (IfMax 2 (Tall 1 (3/100) (1/2)) Grid) |||
     renamed [Replace "ThreeColMid"]    (ThreeColMid 1 (3/100) (1/2))         |||
     renamed [Replace "Dishes"]         (StackTile 2 (3/100) (5/6))           |||
@@ -31,7 +29,9 @@ myLayouts = draggingVisualizer $ maximize $
     renamed [Replace "Full"]           (tabbed shrinkText tabbedTheme)       |||
     renamed [Replace "Dwindle"]        (Dwindle R CW 1 1.1)                  |||
     renamed [Replace "Mirror Dwindle"] (Mirror $ Dwindle R CW 1 1.1)
-    where
+
+spacing = spacingRaw True (Border 0 gapsize gapsize gapsize) True (Border gapsize gapsize gapsize gapsize) True
+    where gapsize = 4
 
 tabbedTheme = def { fontName            = "xft:FantasqueSansMono Nerd Font:size=12"
                   , activeColor         = "#707070"
