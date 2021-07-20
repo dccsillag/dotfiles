2faba8cf429e2d08281a044dcbc114f745525b9e
Daniel Csillag <dccsillag@gmail.com>
2021-07-20 18:38:20 -0300

    Change theming in XMonad; Change completion key in XMonad prompt to tab

--- .xmonad/src/XMonad/Csillag/Consts.hs
+++ .xmonad/src/XMonad/Csillag/Consts.hs
@@ -24,21 +24,39 @@ brightnessDownIcon = "\xf5de - "
 
 workspaceTempFile = "/tmp/.xmonad_workspaces"
 
--- Window gap size
+-- Simple global configs
 
 gapsize :: Integer
 gapsize = 4
+
+uiFont :: String
+uiFont = "xft:FantasqueSansMono Nerd Font:size=18:antialias=true:autohint=True"
+
+uiBorderColor :: String
+uiBorderColor = "#2e343f"
+
+uiNormalBgColor, uiNormalFgColor :: String
+uiNormalBgColor = "#282C33"
+uiNormalFgColor = "#9cb0ba"
+
+uiPrimaryBgColor, uiPrimaryFgColor :: String
+uiPrimaryBgColor = "#7F9BC7"
+uiPrimaryFgColor = "#121212"
+
+uiSecondaryBgColor, uiSecondaryFgColor :: String
+uiSecondaryBgColor = "#31363f"
+uiSecondaryFgColor = "#9cb0ba"
 
 -- Prompt config
 
 csillagPromptConfig :: XPConfig
 csillagPromptConfig =
-    XPC { font                 = "xft:FantasqueSansMono Nerd Font:size=18:antialias=true:autohint=True"
-        , bgColor              = "#282C33"
-        , fgColor              = "#9cb0ba"
-        , bgHLight             = "#00e896" -- TODO
-        , fgHLight             = "#000000" -- TODO
-        , borderColor          = "#2e343f"
+    XPC { font                 = uiFont
+        , bgColor              = uiNormalBgColor
+        , fgColor              = uiNormalFgColor
+        , bgHLight             = uiPrimaryBgColor
+        , fgHLight             = uiPrimaryFgColor
+        , borderColor          = uiBorderColor
         , promptBorderWidth    = 1
         , position             = CenteredAt 0.25 0.5
         , alwaysHighlight      = True
@@ -48,7 +66,7 @@ csillagPromptConfig =
         , historySize          = 0
         , historyFilter        = historyFilter def
         , promptKeymap         = vimLikeXPKeymap' id ("[N] "++) (filter isPrint) isSpace
-        , completionKey        = (controlMask, xK_n)
+        , completionKey        = (0, xK_Tab)
         , changeModeKey        = xK_F1
         , defaultText          = ""
         , autoComplete         = Just 400_000
@@ -61,26 +79,29 @@ csillagPromptConfig =
 
 -- GridSelect config
 
-myGridSelectConfig = (buildDefaultGSConfig colorizer) { gs_cellheight   = round size
-                                                      , gs_cellwidth    = round $ silver * size
-                                                      , gs_cellpadding  = 30
-                                                      , gs_font         = "xft:Lato-Regular:pixelsize=20"
-                                                      , gs_originFractX = 0.5
-                                                      , gs_originFractY = 0.5
-                                                      , gs_bordercolor  = "#666666"
-                                                      }
+myGridSelectConfig = GSConfig { gs_cellheight   = round size
+                              , gs_cellwidth    = round $ silver * size
+                              , gs_cellpadding  = 30
+                              , gs_colorizer    = colorizer
+                              , gs_font         = uiFont
+                              , gs_navigate     = gs_navigate def
+                              , gs_rearranger   = gs_rearranger def
+                              , gs_originFractX = 0.5
+                              , gs_originFractY = 0.5
+                              , gs_bordercolor  = uiBorderColor
+                              }
     where size   = 150.0 :: Double
           silver = 2.4142135623 :: Double
           colorizer :: WorkspaceId -> Bool -> X (String, String)
           colorizer wks active = withWindowSet $ \ws -> do
               let curWkss = map W.tag $ W.workspace (W.current ws) : map W.workspace (W.visible ws)
-              return $ if wks `elem` curWkss
-                          then if active then ("#aaaaaa", "#000000") else ("#777777", "#000000")
-                          else if active then ("#ffffff", "#000000") else ("#000000", "#ffffff")
+              return $ if active then (uiPrimaryBgColor, uiPrimaryFgColor)
+                                 else if wks `elem` curWkss then (uiSecondaryBgColor, uiSecondaryFgColor)
+                                                            else ( uiNormalBgColor,  uiNormalFgColor)
 
 -- Tabbed config
 
-tabbedTheme = def { fontName            = "xft:FantasqueSansMono Nerd Font:size=12"
+tabbedTheme = def { fontName            = uiFont
                   , activeColor         = "#707070"
                   , activeTextColor     = "#ffffff"
                   , activeBorderColor   = "#eeeeee"
