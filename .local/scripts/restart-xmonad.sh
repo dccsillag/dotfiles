da604b0c3eed817f0d5441e755ed138d8cc3bc58
Daniel Csillag <dccsillag@gmail.com>
2021-07-20 10:59:46 -0300

    Change restart-xmonad script to show progress in a Dunst notification

--- .local/scripts/restart-xmonad.sh
+++ .local/scripts/restart-xmonad.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
 
-if xmonad --recompile
+cd ~/.xmonad || { notify-send -u critical XMonad 'Could not cd into `~/.xmonad`!!'; exit 1; }
+
+# Random, but unique ID:
+MSGID=483972
+
+notify() {
+    if [ -n "$2" ]
+    then
+        urgency="$2"
+    else
+        urgency=low
+    fi
+    dunstify -a xmonad-recompilation -r "$MSGID" -u "$urgency" "Recompiling XMonad" "$1"
+}
+
+notify "Starting..."
+
+SUCESS_FILE="$HOME/.xmonad/recompilation-success"
+
+{ stack build 2>&1 && touch "$SUCESS_FILE"; } | ( while IFS="" read -r line
+do
+    ( echo "$line" | grep -q "^[A-Za-z\[]" ) && notify "$line"
+    # just to close the bracket: ]
+done )
+
+if [ -f "$SUCESS_FILE" ]
 then
     xmonad --restart
-    notify-send -u low XMonad "Restarted."
+    notify "Successfully restarted." normal
 else
-    notify-send -u critical XMonad "Compilation failed."
+    notify "Compilation failed." critical
 fi
+
+rm -f "$SUCESS_FILE"
