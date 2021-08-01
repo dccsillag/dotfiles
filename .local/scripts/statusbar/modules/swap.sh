b01501d1ea3e9cd429a3f37ca81fb93e954c2e85
Daniel Csillag <dccsillag@gmail.com>
2021-08-01 20:05:33 -0300

    Fix statusbar 'swap' module to work on a system with zram instead of /swapfile

--- .local/scripts/statusbar/modules/swap.sh
+++ .local/scripts/statusbar/modules/swap.sh
@@ -2,7 +2,7 @@
 
 while sleep 1
 do
-    swaps=$(swapon --show --bytes --raw | grep '^/swapfile\>')
+    swaps=$(swapon --show --bytes --raw | grep '^/swapfile\>\|zram')
     total_swap=$(echo "$swaps" | cut -d' ' -f3)
     used_swap=$(echo "$swaps" | cut -d' ' -f4)
     swap_usage=$((100*used_swap / total_swap))
