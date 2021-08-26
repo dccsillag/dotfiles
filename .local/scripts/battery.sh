069ce313a258ac11b312f69fa8344139af7e83b3
Daniel Csillag <dccsillag@gmail.com>
2021-08-26 09:25:41 -0300

    Fix battery script for when there isn't a battery

--- .local/scripts/battery.sh
+++ .local/scripts/battery.sh
@@ -2,8 +2,24 @@
 
 batpath="$(find /sys/class/power_supply/ -iname '*bat*')"
 
+has_battery() {
+    [ -n "$batpath" ]
+}
+
 case "$1" in
-    status) cat "$batpath"/status ;;
-    charge) cat "$batpath"/capacity ;;
+    status)
+        if has_battery
+        then
+            cat "$batpath"/status
+        else
+            echo "Charging"
+        fi ;;
+    charge)
+        if has_battery
+        then
+            cat "$batpath"/capacity
+        else
+            echo "100"
+        fi ;;
     *)      echo "bad command: must be either 'status' or 'charge'." 1>&2; exit 2 ;;
 esac
