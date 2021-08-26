486f7ba504bf8fcc86b37706410b2de59d0d9f50
Daniel Csillag <dccsillag@gmail.com>
2021-08-26 09:26:05 -0300

    Refactor qutebrowser-quick script to not depend on a specific executable location

--- .local/scripts/qutebrowser-open.sh
+++ .local/scripts/qutebrowser-open.sh
@@ -20,11 +20,10 @@ fi
 _qb_version='1.0.4'
 _proto_version=1
 _ipc_socket="${XDG_RUNTIME_DIR}/qutebrowser/ipc-$(printf '%s' "$USER" | md5sum | cut -d' ' -f1)"
-_qute_bin="/usr/bin/qutebrowser"
 
 printf '{"args": ["%s"], "target_arg": %s, "version": "%s", "protocol_version": %d, "cwd": "%s"}\n' \
        "${_url}" \
        "${_target}" \
        "${_qb_version}" \
        "${_proto_version}" \
-       "${PWD}" | socat - UNIX-CONNECT:"${_ipc_socket}" 2>/dev/null || "$_qute_bin" "$@" &
+       "${PWD}" | socat - UNIX-CONNECT:"${_ipc_socket}" 2>/dev/null || qutebrowser "$@" &
