248ecabf437b446bb94a5ba5d3b98a74bd76adc1
Daniel Csillag <dccsillag@gmail.com>
2021-09-29 09:02:47 -0300

    Fix statusbar swap module to now show all swap

--- .local/scripts/statusbar/modules/swap.sh
+++ .local/scripts/statusbar/modules/swap.sh
@@ -2,7 +2,7 @@
 
 while sleep 1
 do
-    swaps=$(swapon --show --bytes --raw | grep '^/swapfile\>\|zram')
+    swaps=$(swapon --show --bytes --raw | tail +2)
     total_swap=$(echo "$swaps" | cut -d' ' -f3)
     used_swap=$(echo "$swaps" | cut -d' ' -f4)
     swap_usage=$((100*used_swap / total_swap))
