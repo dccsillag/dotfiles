349e84fb032ce2089f27c23da81e8eac4cbee66f
Daniel Csillag <dccsillag@gmail.com>
2022-02-01 18:00:26 -0300

    Change linux kernel to latest stable (5.16)
    This solves all the driver/hardware issues.

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = [ config.boot.kernelPackages.rtw89 ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "lanczos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
