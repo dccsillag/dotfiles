11fa1c44d38d6fbf0dc0de74bd60db2e41ef1268
Daniel Csillag <dccsillag@gmail.com>
2022-02-20 19:01:58 -0300

    Change bgset script to exit out if running from rsync

--- .local/scripts/bgset.sh
+++ .local/scripts/bgset.sh
@@ -5,6 +5,9 @@ is_number() {
     return $?
 }
 
+# If running from rsync, do nothing.
+{ ps -o ppid= | xargs -L1 ps -o comm= -p | grep rsync >/dev/null 2>&1; } && exit 0
+
 if is_number "$1"; then
     printf '\033]11;#%s\a' "$1"
 else
