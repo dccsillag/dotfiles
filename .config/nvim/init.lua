1955cdfea6bacbbdfea63cfd3164ef813ca6e720
Daniel Csillag <dccsillag@gmail.com>
2022-03-23 22:40:26 -0300

    Add proper automatic compilation of Yuescript and PackerCompile in neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,14 +8,25 @@ local function file_exists(path)
     end
 end
 
+--- Get the last modification timestamp for the given file, given it exists.
+local function get_file_timestamp(path)
+    local f = io.popen("stat -c %Y " .. path)
+    local timestamp = tonumber(f:read())
+    f:close()
+
+    return timestamp
+end
+
+RECOMPILED_SOMETHING = false
 local function require_yuescript(module)
     local base_path = vim.fn.expand "~/.config/nvim/lua/csillag/"
 
     local lua_file = base_path .. module .. ".lua"
     local yue_file = base_path .. module .. ".yue"
-    if not file_exists(lua_file) then -- TODO or if the yue file is newer
-        print("`" .. lua_file .. "` not found, compiling it from `" .. yue_file .. "`")
+    if not file_exists(lua_file) or get_file_timestamp(yue_file) > get_file_timestamp(lua_file) then
+        print("Compiling `" .. yue_file .. "` into `" .. lua_file .. "`")
         os.execute("yue " .. yue_file)
+        RECOMPILED_SOMETHING = true
     end
     return require("csillag." .. module)
 end
@@ -25,4 +36,7 @@ require_yuescript "config"
 require_yuescript "misc"
 require_yuescript "mappings"
 
--- TODO auto PackerCompile
+if RECOMPILED_SOMETHING then
+    print "Running :PackerCompile"
+    vim.cmd "PackerCompile"
+end
