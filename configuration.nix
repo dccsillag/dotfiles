903c920ef69eda24effdc8d3dffc7e322bba2018
Daniel Csillag <dccsillag@gmail.com>
2022-05-06 20:19:22 -0300

    Add wacom.enable = true setting

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,7 @@ in {
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
+  services.xserver.wacom.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.daniel = {
