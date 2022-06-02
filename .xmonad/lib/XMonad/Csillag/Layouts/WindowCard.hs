226ab65d53b0e97f1e80af59428acc3b55f7e945
Daniel Csillag <dccsillag@gmail.com>
2022-06-02 17:09:07 -0300

    Add start of functional buttons in WindowCard

--- .xmonad/lib/XMonad/Csillag/Layouts/WindowCard.hs
+++ .xmonad/lib/XMonad/Csillag/Layouts/WindowCard.hs
@@ -1,21 +1,32 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, BlockArguments #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, BlockArguments, MultiWayIf, ViewPatterns #-}
 
 module XMonad.Csillag.Layouts.WindowCard
     ( windowCard
     )
 where
 
-import Data.List ((\\))
+import Data.List ((\\), stripPrefix)
 import Control.Monad
-import qualified Data.Map as M
-import Data.Map (Map)
+import qualified Data.Bimap as M
+import Data.Bimap (Bimap)
 import XMonad
 import qualified XMonad.StackSet as W
 import XMonad.Layout.LayoutModifier
 import XMonad.Util.XUtils
 
 
-data WindowCard a = WindowCard { windowcard_windowmap :: (Map Window Window), windowcard_currentwindow :: Maybe Window } deriving (Show, Read)
+instance (Read a, Read b, Ord a, Ord b) => Read (Bimap a b) where
+    readsPrec p = readParen (p > 10) $ \r -> do
+        ("fromList",s) <- lex r
+        (xs,t) <- reads s
+        return (M.fromList xs, t)
+
+newtype DecorationWindow = DecorationWindow { xwin :: Window } deriving (Show, Read, Eq, Ord)
+
+data WindowCard a = WindowCard
+    { windowcard_windowmap :: (Bimap Window DecorationWindow)
+    , windowcard_currentwindow :: Maybe Window
+    } deriving (Read, Show)
 windowCard = ModifiedLayout $ WindowCard M.empty Nothing
 
 instance LayoutModifier WindowCard Window where
@@ -27,18 +38,19 @@ instance LayoutModifier WindowCard Window where
 
         -- remove and delete decoration windows that ought to be removed:
         let windowmap' = foldl (flip M.delete) windowmap $ map fst windows_to_remove
-        deleteWindows $ map snd windows_to_remove
+        deleteWindows $ map (xwin.snd) windows_to_remove
         -- create and add decoration windows that ought to be created:
         new_windows <- forM windows_to_add $ \(_, rect@(Rectangle _ _ width height)) -> do
-            w <- createNewWindow rect Nothing "#000000" True
+            let rect' = rect { rect_width = barSize }
+            w <- createNewWindow rect' (Just $ exposureMask .|. buttonPressMask) "#000000" True
             d <- asks display
             io $ setClassHint d w $ ClassHint "xmonad-decoration" "xmonad"
 
-            return w
+            return $ DecorationWindow w
         let windowmap'' = foldl (\acc (w, w') -> M.insert w w' acc) windowmap' $ zip (fst <$> windows_to_add) new_windows
 
         -- finally, assemble everything together for the return
-        let window_rects' = window_rects >>= (\(w, r) -> [(w, shrinkWindowRectangle r), (windowmap'' M.! w, r { rect_width = barSize })])
+        let window_rects' = window_rects >>= (\(w, r) -> [(w, shrinkWindowRectangle r), (xwin $ windowmap'' M.! w, r { rect_width = barSize })])
         let layout' = WindowCard windowmap'' $ fmap W.focus stack
         return (window_rects', Just layout')
 
@@ -47,20 +59,34 @@ instance LayoutModifier WindowCard Window where
             handleEvent windowmap current_window e
             return Nothing
         | Just Hide <- fromMessage message = do
-            hideWindows $ M.elems windowmap
+            hideWindows $ map xwin $ M.elems windowmap
             return Nothing
         | Just ReleaseResources <- fromMessage message = do
-            deleteWindows $ M.elems windowmap
+            deleteWindows $ map xwin $ M.elems windowmap
             return $ Just $ WindowCard M.empty current_window
         | otherwise = return Nothing
 
-handleEvent :: Map Window Window -> Maybe Window -> Event -> X ()
+handleEvent :: Bimap Window DecorationWindow -> Maybe Window -> Event -> X ()
 handleEvent windowmap current_window event
-  | PropertyEvent {ev_window = w} <- event, Just w' <- M.lookup w windowmap = redrawWindow (Just w == current_window) w'
-  | ExposeEvent   {ev_window = w} <- event, Just w' <- M.lookup w windowmap = redrawWindow (Just w == current_window) w'
+    | PropertyEvent{ev_window = w} <- event, Just w' <- M.lookup w windowmap
+        = redrawWindow (Just w == current_window) w'
+    | ExposeEvent{ev_window = w} <- event, Just w' <- M.lookup w windowmap
+        = redrawWindow (Just w == current_window) w'
+    | ButtonEvent{ev_window = w', ev_event_type = et, ev_y = ey} <- event
+    , et == buttonPress
+    , Just w <- M.lookupR (DecorationWindow w') windowmap
+        = if
+            | 1*buttonSpacing + 0*fi buttonSize <= ey && ey <= 2*buttonSpacing + 1*fi buttonSize
+                -> spawn "notify-send windowcard 1"
+            | 2*buttonSpacing + 1*fi buttonSize <= ey && ey <= 3*buttonSpacing + 2*fi buttonSize
+                -> spawn "notify-send windowcard 2"
+            | 3*buttonSpacing + 2*fi buttonSize <= ey && ey <= 4*buttonSpacing + 3*fi buttonSize
+                -> spawn "notify-send windowcard 3"
+            | otherwise -> return ()
+    | otherwise = return ()
 
-redrawWindow :: Bool -> Window -> X ()
-redrawWindow is_current w = do
+redrawWindow :: Bool -> DecorationWindow -> X ()
+redrawWindow is_current (DecorationWindow w) = do
     d <- asks display
     gc <- io $ createGC d w
     io $ setGraphicsExposures d gc False
@@ -71,8 +97,8 @@ redrawWindow is_current w = do
     colorGreen <- stringToPixel d "#3cb44b"
 
     let button i color = io $ do
-        let x = fi $ (barSize - buttonSize) `div` 2 + 1
-        let y = buttonSpacing*(i+1) + fi buttonSize*i
+        let x = fi $ (barSize - buttonSize) `div` 2
+        let y = fi $ buttonSpacing*(i+1) + fi buttonSize*i
 
         setForeground d gc color
         -- fillRectangle d w gc x y buttonSize buttonSize
