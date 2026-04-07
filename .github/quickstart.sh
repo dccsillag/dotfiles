949ad4d09081ab7f0c8efa5cbb186b812ff1fab3
Daniel Csillag <dccsillag@gmail.com>
2026-04-07 18:34:00 -0300

    Update quickstart script

--- .github/quickstart.sh
+++ .github/quickstart.sh
@@ -1,13 +1,30 @@
 #!/bin/sh -ex
 
-git clone --bare https://github.com/dccsillag/dotfiles.git ~/.dotfiles.git
+cd ~
+
+export TERM=xterm-256color  # make sure we have a widely used terminfo
+
+git clone --bare git@github.com:dccsillag/dotfiles.git ~/.dotfiles.git
 git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME checkout -f
 git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME push --set-upstream origin master
 git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME config core.bare false
 git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME config core.worktree $HOME
 git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME config status.showUntrackedFiles no
 . .profile
-sh -c "$(curl https://raw.githubusercontent.com/dccsillag/packs/main/packs.sh)"
+
+curl https://mise.run | sh
+mise install
+
+if ! command -v zsh >/dev/null 2>&1
+then
+    # install zsh:
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)"
+
+    if [ ! -f "$HOME/.local/bin/zsh" ]
+    then
+        ( echo '$HOME/.local/bin/zsh'; echo 'exit $?' ) >> ~/.bash_profile  # TODO make this more seamless
+    do
+fi
 
 echo
 echo
